bc3d2393435cc728e640d1f08f214bfe1c72ee71
sanjay-2605 <161396396+sanjay-2605@users.noreply.github.com>
2026-04-07 09:49:00 +0530

    Delete utils directory

--- utils/utils/utils/llm.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-client = OpenAI()
-
-def generate_response(text):
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[{"role": "user", "content": text}]
-    )
-    return response.choices[0].message.content
